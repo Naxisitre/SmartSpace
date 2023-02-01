96c294166eb5bd00c9ae51aaf8f637e16dbefa7d
Alexis LEHE <alexis.lehe@outlook.fr>
2023-02-01 09:17:46 +0100

    initial commit

---
+++ app/src/main/java/iutinfo/lp/devmob/smartspace/MainActivity.kt
@@ -0,0 +1,11 @@
+package iutinfo.lp.devmob.smartspace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
