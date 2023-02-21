ff72e4f5d08d8cbf9d8298ec4424c70d7331d5bf
Alexis LEHE <alexis.lehe@outlook.fr>
2023-02-21 11:34:12 +0100

    (+) (components) création d'une view pour le composant température

---
+++ app/src/main/java/iutinfo/lp/devmob/smartspace/TemperatureView.kt
@@ -0,0 +1,17 @@
+package iutinfo.lp.devmob.smartspace
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.TextView
+
+class TemperatureView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+
+    init {
+        inflate(context, R.layout.temperature_view, this)
+    }
+
+    fun setTemp(temp: Int) {
+        findViewById<TextView>(R.id.textView3).setText("°C")
+    }
+}
