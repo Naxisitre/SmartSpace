71db8a920fdc887510b084c677fb7fd7b5f70ebb
Alexis LEHE <alexis.lehe@outlook.fr>
2023-02-28 15:25:16 +0100

    création du test pour vérifier si l'appareil supporte ou non le NFC et si il est désactivé ou non

--- app/src/main/java/iutinfo/lp/devmob/smartspace/MainActivity.kt
+++ app/src/main/java/iutinfo/lp/devmob/smartspace/MainActivity.kt
@@ -5,30 +5,38 @@ import android.app.AlertDialog
 import android.app.PendingIntent
 import android.content.Intent
 import android.nfc.NfcAdapter
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
-import android.view.View
-import androidx.lifecycle.Observer
+import android.view.View.GONE
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
     private var nfcAdapter: NfcAdapter? = null
     private var pendingIntent: PendingIntent? = null
+
     private var handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-
         val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-        //this.nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-       // pendingIntent = PendingIntent.getActivity(this, 0, Intent(this, this.javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
+        this.nfcAdapter = NfcAdapter.getDefaultAdapter(this)
 
+        if (nfcAdapter == null) {
+            findViewById<TextView>(R.id.button_prob).visibility = GONE
+        }
+        /*pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, this.javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            PendingIntent.FLAG_IMMUTABLE
+        )*/
         refreshData(viewModel)
         refreshDataLoop(viewModel)
     }
@@ -61,8 +69,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onProblemClicked(view: View) {
-        AlertDialog.Builder(this).setView(R.layout.popupnfc_view).show()
+    fun onProblemClicked() {
+        val dialog = AlertDialog.Builder(this).setView(R.layout.popupnfc_view).show()
+        var intent: Intent?
+        if(!nfcAdapter!!.isEnabled){
+            dialog.findViewById<TextView>(R.id.title).text = resources.getString(R.string.nfc_text_state, "désactivé")
+            dialog.findViewById<TextView>(R.id.text_nfc).text = resources.getString(R.string.info_comp_nfc, "Vous devez activer le NFC pour vous identifier avec votre carte izly")
+            dialog.findViewById<TextView>(R.id.active_nfc).text = resources.getString(R.string.active_nfc, "Activer le NFC")
+            dialog.findViewById<TextView>(R.id.active_nfc).setOnClickListener {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
+                     intent = Intent(android.provider.Settings.ACTION_NFC_SETTINGS)
+                    startActivity(intent)
+                }
+                else {
+                    intent = Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS)
+                    startActivity(intent)
+                }
+            }
+        }
+        else if(nfcAdapter!!.isEnabled){
+            dialog.findViewById<TextView>(R.id.title).text = resources.getString(R.string.nfc_text_state, "activé")
+            dialog.findViewById<TextView>(R.id.text_nfc).text = resources.getString(R.string.info_comp_nfc, "placer votre carte izly sur votre téléphone pour vous identifier")
+            dialog.findViewById<TextView>(R.id.active_nfc).visibility = GONE
+        }
     }
 
 }
