75763620cb2b781596dda20fc1c6f6608927fc40
Alexis LEHE <alexis.lehe@outlook.fr>
2023-03-03 16:21:02 +0100

    Ajout des retour utilisateurs pour l'envoie d'un problème

--- app/src/main/java/iutinfo/lp/devmob/projetsmartspace/ProblemeActivity.kt
+++ app/src/main/java/iutinfo/lp/devmob/projetsmartspace/ProblemeActivity.kt
@@ -1,6 +1,7 @@
 package iutinfo.lp.devmob.projetsmartspace
 
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
@@ -10,9 +11,11 @@ import android.util.Log
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import java.io.File
@@ -49,5 +52,39 @@ class ProblemeActivity() : AppCompatActivity() {
             photoFile
         )
         takePicture.launch(uri)
+    }
+
+    fun EnvoieProbleme(view: View) {
+        val textDesc = findViewById<EditText>(R.id.desc_prob_text).text.toString()
+        if(uri != null && textDesc != "") {
+            Log.i("Uri", uri.toString())
+            Log.i("Text", textDesc)
+            val dialog = AlertDialog.Builder(this).setView(R.layout.popupnfc_view).show()
+            dialog.findViewById<TextView>(R.id.title)?.text= "Problème envoyé"
+            dialog.findViewById<TextView>(R.id.text_nfc)!!.visibility = GONE
+            dialog.findViewById<TextView>(R.id.active_nfc)!!.visibility = GONE
+            dialog.setOnDismissListener{
+                val intentProbleme = Intent(this, MainActivity::class.java)
+                startActivity(intentProbleme)
+            }
+        }
+        else if(uri == null && textDesc != ""){
+            val dialog = AlertDialog.Builder(this).setView(R.layout.popupnfc_view).show()
+            dialog.findViewById<TextView>(R.id.title)?.text= "Veuillez prendre une photo"
+            dialog.findViewById<TextView>(R.id.text_nfc)!!.visibility = GONE
+            dialog.findViewById<TextView>(R.id.active_nfc)!!.visibility = GONE
+        }
+        else if(uri != null && textDesc == ""){
+            val dialog = AlertDialog.Builder(this).setView(R.layout.popupnfc_view).show()
+            dialog.findViewById<TextView>(R.id.title)?.text= "Veuillez décrire le problème"
+            dialog.findViewById<TextView>(R.id.text_nfc)!!.visibility = GONE
+            dialog.findViewById<TextView>(R.id.active_nfc)!!.visibility = GONE
+        }
+        else{
+            val dialog = AlertDialog.Builder(this).setView(R.layout.popupnfc_view).show()
+            dialog.findViewById<TextView>(R.id.title)?.text= "Veuillez prendre une photo et décrire le problème"
+            dialog.findViewById<TextView>(R.id.text_nfc)!!.visibility = GONE
+            dialog.findViewById<TextView>(R.id.active_nfc)!!.visibility = GONE
+        }
     }
 }
