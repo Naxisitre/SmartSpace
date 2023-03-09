1f593d86d243fc4a6803872293ec1d9b7c26c880
Alexis LEHE <alexis.lehe@outlook.fr>
2023-03-09 23:22:12 +0100

    Ajouts des list view pour voir les problèmes

--- app/src/main/java/iutinfo/lp/devmob/projetsmartspace/ProblemViewActivity.kt
+++ app/src/main/java/iutinfo/lp/devmob/projetsmartspace/ProblemViewActivity.kt
@@ -1,26 +1,57 @@
 package iutinfo.lp.devmob.projetsmartspace
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ListView
 import androidx.lifecycle.ViewModelProvider
+import iutinfo.lp.devmob.projetsmartspace.API.GetDataService
 import iutinfo.lp.devmob.projetsmartspace.ViewModel.ProblemViewActivityViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class ProblemViewActivity : AppCompatActivity() {
     var viewModel = ProblemViewActivityViewModel()
+    var listViewEnCours: ListView? = null
+    var listViewAtraiter: ListView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_problem_view)
         supportActionBar?.hide()
         viewModel = ViewModelProvider(this).get(ProblemViewActivityViewModel::class.java)
-        viewModel.listViewEnCours = findViewById(R.id.listView_encours)
-        viewModel.listViewAtraiter = findViewById(R.id.listView_atraiter)
-        viewModel.getProblem()
-        viewModel.context = this
+        listViewEnCours = findViewById(R.id.listView_encours)
+        listViewAtraiter = findViewById(R.id.listView_atraiter)
+        executeCall(this)
     }
 
+    private fun executeCall(context: Context) {
+        GlobalScope.launch(Dispatchers.Main){
+            try {
+                val myResponse = GetDataService.retrofit.getAllProblems()
+                if(myResponse.isSuccessful){
+                    val responseBody = myResponse.body()
+                    Log.i("Response", responseBody.toString())
+                    if(responseBody != null) {
+                        for (i in responseBody) {
+                            if (i.etat == "à traiter") {
+                                    listViewAtraiter?.adapter = ProblemViewAdapter(context, responseBody)
+                            } else if (i.etat == "En Cours") {
+                                    listViewEnCours?.adapter = ProblemViewAdapter(context, responseBody)
+                                }
+                            }
+                        }
+                    }
+                }
+            catch (e: Exception) {
+                e.printStackTrace()
+        }
+    }
+}
     fun retourAcceuil(view: View) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
--- app/src/main/java/iutinfo/lp/devmob/projetsmartspace/ViewModel/ProblemViewActivityViewModel.kt
+++ app/src/main/java/iutinfo/lp/devmob/projetsmartspace/ViewModel/ProblemViewActivityViewModel.kt
@@ -1,48 +1,42 @@
 package iutinfo.lp.devmob.projetsmartspace.ViewModel
 
-import android.content.Context
-import android.util.Log
-import android.widget.ListView
+
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import iutinfo.lp.devmob.projetsmartspace.API.GetDataService
-import iutinfo.lp.devmob.projetsmartspace.ProblemViewAdapter
-import kotlinx.coroutines.launch
 
 
 class ProblemViewActivityViewModel: ViewModel() {
-    var listViewEnCours: ListView? = null
-    var listViewAtraiter: ListView? = null
-    var context: Context? = null
+    //var listViewEnCours: ListView? = null
+    //var listViewAtraiter: ListView? = null
+    //var context: Context? = null
     var etat: String? = null
-    fun getProblem() {
+    /*  fun getProblem() {
         try {
-            viewModelScope.launch {
-                val myResponse = GetDataService.retrofit.getAllProblems()
-                val responseBody = myResponse.body()
-                Log.i("Response", responseBody.toString())
-                if(responseBody != null) {
-                    listViewEnCours?.adapter = context?.let { ProblemViewAdapter(it, responseBody) }
-                    /*for (i in responseBody) {
-                        etat = i.etat
-                    }*/
-                    //Log.i("ResponseEtat", responseBody[0].etat.toString())
-                    /*when (etat) {
-                        "à traiter" -> {
-                            listViewAtraiter?.adapter =
-                                context?.let { ProblemViewAdapter(it, responseBody) }
-                        }
-                        "en cours" -> {
-                            listViewEnCours?.adapter =
-                                context?.let { ProblemViewAdapter(it, responseBody) }
-                        }
-                    }*/
-                }
-                    //listViewEnCours?.adapter = context?.let { ProblemViewAdapter(it, responseBody) }
-                }
-        }
-        catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
+             viewModelScope.launch {
+                 val myResponse = GetDataService.retrofit.getAllProblems()
+                 val responseBody = myResponse.body()
+                 Log.i("Response", responseBody.toString())
+                 if(responseBody != null) {
+                     listViewEnCours?.adapter = context?.let { ProblemViewAdapter(it, responseBody) }
+                     /*for (i in responseBody) {
+                         etat = i.etat
+                     }*/
+                     //Log.i("ResponseEtat", responseBody[0].etat.toString())
+                     /*when (etat) {
+                         "à traiter" -> {
+                             listViewAtraiter?.adapter =
+                                 context?.let { ProblemViewAdapter(it, responseBody) }
+                         }
+                         "en cours" -> {
+                             listViewEnCours?.adapter =
+                                 context?.let { ProblemViewAdapter(it, responseBody) }
+                         }
+                     }*/
+                 }
+                     //listViewEnCours?.adapter = context?.let { ProblemViewAdapter(it, responseBody) }
+                 }
+         }
+         catch (e: Exception) {
+             e.printStackTrace()
+         }
+    }*/
 }
